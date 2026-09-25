b0c5b012a17e6f08b49ada778cd9485ed349a311
Victor <haller_00@hotmail.com>
2026-09-25 12:20:27 -0500

    2do versión del proyecto

--- ana_config/settings.py
+++ ana_config/settings.py
@@ -128,3 +128,7 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/monitoreo/proyectos/"
+LOGOUT_REDIRECT_URL = "/login/"
